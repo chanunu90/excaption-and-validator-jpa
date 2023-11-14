fd16ef53dde37fe5117d2e3c14a13720d77370b2
chanunu90 <vltfhem1206@gmail.com>
2023-11-14 17:34:17 +0900

    jpa and exception validator

---
+++ src/main/java/com/boot3/myrestapi/lectures/LectureController.java
@@ -0,0 +1,60 @@
+package com.boot3.myrestapi.lectures;
+
+import com.boot3.myrestapi.lectures.dto.LectureReqDto;
+import com.boot3.myrestapi.lectures.validator.LectureValidator;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/api/lectures", produces = MediaTypes.HAL_JSON_VALUE)
+@RequiredArgsConstructor
+public class LectureController {
+    private final LectureRepository lectureRepository;
+    private final ModelMapper modelMapper;
+    private final LectureValidator lectureValidator;
+
+    //Constructor Injection
+//    public LectureController(LectureRepository lectureRepository) {
+//        this.lectureRepository = lectureRepository;
+//    }
+
+    @PostMapping
+    public ResponseEntity<?> createLecture(@RequestBody @Valid LectureReqDto lectureReqDto,
+                                           Errors errors) {
+        //입력항목 검증
+        if(errors.hasErrors()) {
+            //status code 400
+            return getErrors(errors);
+        }
+        //biz로직과 관련된 입력항목 검증
+        this.lectureValidator.validate(lectureReqDto, errors);
+        if(errors.hasErrors()) {
+            return getErrors(errors);
+        }
+
+        Lecture lecture = modelMapper.map(lectureReqDto, Lecture.class);
+        //offline, free 값을 update
+        lecture.update();
+        Lecture addLecture = this.lectureRepository.save(lecture);
+
+        WebMvcLinkBuilder selfLinkBuilder =
+                WebMvcLinkBuilder.linkTo(LectureController.class).slash(addLecture.getId());
+        URI createUri = selfLinkBuilder.toUri();
+        return ResponseEntity.created(createUri).body(addLecture);
+    }
+
+    private static ResponseEntity<Errors> getErrors(Errors errors) {
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
